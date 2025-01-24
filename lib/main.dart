833a38e03edf5943ccca25d9d588adfdad345563
itrieditlikethis <albert.fedev@gmail.com>
2025-01-24 13:27:00 +0200

    feat: add base project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.cyan,
+                Colors.cyanAccent,
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              spacing: 30,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('dice'),
+                const Text('text'),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('tap to bring dice'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
